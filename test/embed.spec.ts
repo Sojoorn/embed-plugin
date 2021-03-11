d3d1a41986e0ec2de926648002f7f738e27b66bb
Hugo Pérez <hugoa.perezh@outlook.com>
2021-03-11 13:12:46 -0600

    Declaring Embed class to access getInfo method

--- test/embed.spec.ts
+++ test/embed.spec.ts
@@ -1,8 +1,10 @@
 import { Embed } from "../src/Embed";
 
+const embed = new Embed('AIzaSyDqJnKWU6bN4GyjH6IZ9Fpqx7YLh3-CYRI');
+
 describe("Embed", () => {
   it("Verify YouTube Url", () => {
-    expect(Embed.getInfo('https://www.youtube.com/watch?v=jABp-FRUJ48')).toMatchObject({
+    expect(embed.getInfo('https://www.youtube.com/watch?v=jABp-FRUJ48')).toMatchObject({
       id: expect.any(String), 
       source: expect.any(String),
       url: expect.any(String),
@@ -10,7 +12,7 @@ describe("Embed", () => {
     });
   });
   it("Verify Vimeo Url", () => {
-    expect(Embed.getInfo('https://vimeo.com/243244233')).toMatchObject({
+    expect(embed.getInfo('https://vimeo.com/243244233')).toMatchObject({
       id: expect.any(String), 
       source: expect.any(String),
       url: expect.any(String),
@@ -18,7 +20,7 @@ describe("Embed", () => {
     });
   });
   it("Verify Dailymotion Url", () => {
-    expect(Embed.getInfo('https://www.dailymotion.com/video/x7zn3je')).toMatchObject({
+    expect(embed.getInfo('https://www.dailymotion.com/video/x7zn3je')).toMatchObject({
       id: expect.any(String), 
       source: expect.any(String),
       url: expect.any(String),
@@ -26,7 +28,7 @@ describe("Embed", () => {
     });
   });
   it("Verify Drive Url", () => {
-    expect(Embed.getInfo('https://drive.google.com/file/d/10OoKhqUM2ZukFc-2oVYd-MFy9NWArAFH/view')).toMatchObject({
+    expect(embed.getInfo('https://drive.google.com/file/d/10OoKhqUM2ZukFc-2oVYd-MFy9NWArAFH/view')).toMatchObject({
       id: expect.any(String), 
       source: expect.any(String),
       url: expect.any(String),
