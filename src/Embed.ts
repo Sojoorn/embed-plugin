99d212d73ba82177e89083e76e851612877bf0a7
Hugo Pérez <hugoa.perezh@outlook.com>
2021-03-11 13:11:13 -0600

    Embed converted into class

--- src/Embed.ts
+++ src/Embed.ts
@@ -4,19 +4,22 @@ import { VideoInfo } from './interfaces/video-info.interface';
 import { Vimeo } from './Vimeo';
 import { YouTube } from './Youtube';
 import { Drive } from './Drive';
-export namespace Embed {
-    const platforms: { [key: string]: any } = { youtube: YouTube, vimeo: Vimeo, dailymotion: Dailymotion, drive: Drive };
-    export function getInfo(url: string): VideoInfo {
+export class Embed {
+    private platforms: { [key: string]: any };
+    constructor(driveAPI?: string | undefined) {
+        this.platforms = { youtube: YouTube, vimeo: Vimeo, dailymotion: Dailymotion, drive: new Drive(driveAPI) };
+    }
+    getInfo(url: string): VideoInfo {
         const parsedUrl = new URL(url);
-        const currentPlaforms = Object.keys(platforms);
+        const currentPlaforms = Object.keys(this.platforms);
         let id;
         let source;
         let embedUrl;
         for (let i = 0; i < currentPlaforms.length; i++) {
-            id = platforms[currentPlaforms[i]].detect(parsedUrl);
+            id = this.platforms[currentPlaforms[i]].detect(parsedUrl);
             if (id) {
                 source = currentPlaforms[i];
-                embedUrl =  platforms[currentPlaforms[i]].getEmbedUrl(id);
+                embedUrl =  this.platforms[currentPlaforms[i]].getEmbedUrl(id);
                 break;
             }
         }
