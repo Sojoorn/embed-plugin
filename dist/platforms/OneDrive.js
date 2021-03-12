043988210a1a94e0ee38ff609afc23b460d1ab2a
Hugo Pérez <hugoa.perezh@outlook.com>
2021-03-12 14:33:01 -0600

    Verify if OneDrive file is a video

--- dist/platforms/OneDrive.js
+++ dist/platforms/OneDrive.js
@@ -3,49 +3,72 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.OneDrive = void 0;
 var url_1 = require("url");
 var fetch = require('sync-fetch');
+var cheerio = require('cheerio');
 var OneDrive;
 (function (OneDrive) {
-    function detect(url) {
-        if (url.href) {
-            var parsedUrl = void 0;
-            if (url.href.indexOf('1drv.ms/v') > -1) {
-                // return url.searchParams.get('v'); https://1drv.ms/v/s!AkXEMA-H1XcOhgSFiN5DziB0ICS2?e=5qLe8n
-                var url_2 = fetch('https://1drv.ms/v/s!AkXEMA-H1XcOhgUEW8iWLz7HqeX5?e=4GdvzD').url;
-                parsedUrl = new url_1.URL(url_2);
-            }
-            else if (url.href.indexOf('onedrive.live.com')) {
-                parsedUrl = url;
-            }
-            var resId = (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id')) ? parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id') : parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('resid');
-            return resId === null || resId === void 0 ? void 0 : resId.split('!')[0];
+    var isVideo = function (obj) {
+        var fileExtensions = ["3g2", "3gp", "aaf", "asf", "avchd", "avi", "drc", "flv", "m2v", "m4p", "m4v", "mkv", "mng", "mov", "mp2", "mp4", "mpe", "mpeg", "mpg", "mpv", "mxf", "nsv", "ogg", "ogv", "qt", "rm", "rmvb", "roq", "svi", "vob", "webm", "wmv", "yuv"];
+        var resp = fetch("https://onedrive.live.com/redir.aspx?cid=" + obj.cid + "&resid=" + obj.resId + "&authkey=" + obj.authKey).text();
+        var $ = cheerio.load(resp);
+        var file = $('meta[property="og:title"]').attr('content');
+        if (fileExtensions.includes(file.split('.')[1])) {
+            return true;
         }
-        return null;
-    }
-    OneDrive.detect = detect;
-    function getIds(url) {
+        return false;
+    };
+    var parseObj = function (parsedUrl) {
         var _a, _b;
+        var cid = (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id')) ? "0" + ((_a = parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id')) === null || _a === void 0 ? void 0 : _a.split('!')[0]) : "0" + ((_b = parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('resid')) === null || _b === void 0 ? void 0 : _b.split('!')[0]);
+        var authKey = parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('authkey');
+        var resId = (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id')) ? parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id') : parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('resid');
+        var driveObj = { cid: '', authKey: '', resId: '' };
+        if (cid) {
+            driveObj.cid = encodeURIComponent(cid);
+        }
+        if (authKey) {
+            driveObj.authKey = authKey.replace('!', '%21');
+        }
+        if (resId) {
+            driveObj.resId = resId.replace('!', '%21');
+        }
+        return driveObj;
+    };
+    function detect(url) {
         if (url.href) {
             var parsedUrl = void 0;
             if (url.href.indexOf('1drv.ms/v') > -1) {
                 var redirectedUrl = fetch(url.href).url;
                 parsedUrl = new url_1.URL(redirectedUrl);
             }
-            else if (url.href.indexOf('onedrive.live.com')) {
+            else if (url.href.indexOf('onedrive.live.com') > -1) {
                 parsedUrl = url;
             }
-            var cid = (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id')) ? "0" + ((_a = parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id')) === null || _a === void 0 ? void 0 : _a.split('!')[0]) : "0" + ((_b = parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('resid')) === null || _b === void 0 ? void 0 : _b.split('!')[0]);
-            var authKey = parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('authkey');
-            var resId = (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id')) ? parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('id') : parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.searchParams.get('resid');
-            var driveObj = { cid: '', authKey: '', resId: '' };
-            if (cid) {
-                driveObj.cid = encodeURIComponent(cid);
+            if (!parsedUrl) {
+                return null;
             }
-            if (authKey) {
-                driveObj.authKey = authKey.replace('!', '%21');
+            var obj = parseObj(parsedUrl);
+            if (!isVideo(obj)) {
+                return null;
             }
-            if (resId) {
-                driveObj.resId = resId.replace('!', '%21');
+            return obj === null || obj === void 0 ? void 0 : obj.cid;
+        }
+        return null;
+    }
+    OneDrive.detect = detect;
+    function getIds(url) {
+        if (url.href) {
+            var parsedUrl = void 0;
+            if (url.href.indexOf('1drv.ms') > -1) {
+                var redirectedUrl = fetch(url.href).url;
+                parsedUrl = new url_1.URL(redirectedUrl);
             }
+            else if (url.href.indexOf('onedrive.live.com') > -1) {
+                parsedUrl = url;
+            }
+            if (!parsedUrl) {
+                return null;
+            }
+            var driveObj = parseObj(parsedUrl);
             return driveObj;
         }
         return null;
--- src/platforms/OneDrive.ts
+++ src/platforms/OneDrive.ts
@@ -1,21 +1,65 @@
 import { URL } from 'url';
 const fetch = require('sync-fetch');
+const cheerio = require('cheerio');
 
 export namespace OneDrive {
+    const isVideo = function (obj: {resId: string | undefined, authKey: string | undefined, cid: string | undefined}): boolean {
+        const fileExtensions = ["3g2","3gp","aaf","asf","avchd","avi","drc","flv","m2v","m4p","m4v","mkv","mng","mov","mp2","mp4","mpe","mpeg","mpg","mpv","mxf","nsv","ogg","ogv","qt","rm","rmvb","roq","svi","vob","webm","wmv","yuv"];
+
+        const resp = fetch(`https://onedrive.live.com/redir.aspx?cid=${obj.cid}&resid=${obj.resId}&authkey=${obj.authKey}`).text();
+        const $ = cheerio.load(resp);
+        const file = $('meta[property="og:title"]').attr('content');
+        if (fileExtensions.includes(file.split('.')[1])) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    const parseObj = function(parsedUrl: URL): {resId: string, authKey: string, cid: string} {
+        const cid = parsedUrl?.searchParams.get('id') ? `0${parsedUrl?.searchParams.get('id')?.split('!')[0]}` : `0${parsedUrl?.searchParams.get('resid')?.split('!')[0]}`;
+        const authKey = parsedUrl?.searchParams.get('authkey');
+        const resId = parsedUrl?.searchParams.get('id') ? parsedUrl?.searchParams.get('id') : parsedUrl?.searchParams.get('resid');
+        
+        const driveObj = {cid: '', authKey: '', resId: ''};
+
+        if (cid) {
+            driveObj.cid = encodeURIComponent(cid);
+        }
+        
+        if (authKey) {
+            driveObj.authKey = authKey.replace('!', '%21');
+        }
+
+        if (resId) {
+            driveObj.resId = resId.replace('!', '%21');
+        }
+
+        return driveObj;
+    }
+    
     export function detect(url: URL): string | string[] | undefined | null {
         if (url.href) {
             let parsedUrl;
             if (url.href.indexOf('1drv.ms/v') > -1) {
-                // return url.searchParams.get('v'); https://1drv.ms/v/s!AkXEMA-H1XcOhgSFiN5DziB0ICS2?e=5qLe8n
-                const url = fetch('https://1drv.ms/v/s!AkXEMA-H1XcOhgUEW8iWLz7HqeX5?e=4GdvzD').url;
-                parsedUrl = new URL(url);
-            } else if (url.href.indexOf('onedrive.live.com')) {
+                const redirectedUrl = fetch(url.href).url;
+                parsedUrl = new URL(redirectedUrl);
+            } else if (url.href.indexOf('onedrive.live.com') > -1) {
                 parsedUrl = url;
             }
 
-            const resId = parsedUrl?.searchParams.get('id') ? parsedUrl?.searchParams.get('id') : parsedUrl?.searchParams.get('resid');
 
-            return resId?.split('!')[0];
+            if (!parsedUrl) {
+                return null;
+            }
+
+            const obj = parseObj(parsedUrl)
+
+            if (!isVideo(obj)) {
+                return null;
+            }
+
+            return obj?.cid;
         }
 
         return null;
@@ -24,31 +68,19 @@ export namespace OneDrive {
     export function getIds(url: URL) {
         if (url.href) {
             let parsedUrl;
-            if (url.href.indexOf('1drv.ms/v') > -1) {
+            if (url.href.indexOf('1drv.ms') > -1) {
                 const redirectedUrl = fetch(url.href).url;
                 parsedUrl = new URL(redirectedUrl);
-            } else if (url.href.indexOf('onedrive.live.com')) {
+            } else if (url.href.indexOf('onedrive.live.com') > -1) {
                 parsedUrl = url;
             }
 
-            const cid = parsedUrl?.searchParams.get('id') ? `0${parsedUrl?.searchParams.get('id')?.split('!')[0]}` : `0${parsedUrl?.searchParams.get('resid')?.split('!')[0]}`;
-            const authKey = parsedUrl?.searchParams.get('authkey');
-            const resId = parsedUrl?.searchParams.get('id') ? parsedUrl?.searchParams.get('id') : parsedUrl?.searchParams.get('resid');
+            if (!parsedUrl) {
+                return null;
+            }
+
+            const driveObj = parseObj(parsedUrl);
             
-            const driveObj = {cid: '', authKey: '', resId: ''};
-
-            if (cid) {
-                driveObj.cid = encodeURIComponent(cid);
-            }
-            
-            if (authKey) {
-                driveObj.authKey = authKey.replace('!', '%21');
-            }
-
-            if (resId) {
-                driveObj.resId = resId.replace('!', '%21');
-            }
-
             return driveObj;
         }
 
@@ -57,5 +89,5 @@ export namespace OneDrive {
 
     export function getEmbedUrl(obj: {resId: string, authKey: string, cid: string}): string {
         return `//onedrive.live.com/embed?cid=${obj.cid}&resid=${obj.resId}&authkey=${obj.authKey}`;
-    } 
+    }
 }
